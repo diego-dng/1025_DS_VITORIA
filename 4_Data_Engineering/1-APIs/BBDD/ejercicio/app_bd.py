affc6bc31ad3a3c010785eb383ed18d244549bc3
diego-dng <diego.dng.1994@gmail.com>
2026-01-08 10:10:07 +0100

    W11 D2 EJERCICIOS

--- 4_Data_Engineering/1-APIs/BBDD/ejercicio/app_bd.py
+++ 4_Data_Engineering/1-APIs/BBDD/ejercicio/app_bd.py
@@ -1,9 +1,46 @@
 # IMPORT
+import sqlite3
+from flask import Flask, jsonify, request
 
+def bbdd(query):
+    con = sqlite3.connect("books.db")
+    cursor = con.cursor()
+    query
+    resultado = cursor.execute(query).fetchall()
+    con.close()
+    return resultado
+
+app = Flask(__name__)
+app.config["DEBUG"] = True
+
+@app.route("/", methods = ["GET"])
+def main():
+    return "Api libros con BBDD"
 # 0.Ruta para obtener todos los libros
-
+@app.route("/libros", methods = ["GET"])
+def libros():
+    res = bbdd("SELECT * FROM books")
+    return jsonify(res)
 # 1.Ruta para obtener el conteo de libros por autor ordenados de forma descendente
+@app.route("/libros_autor", methods = ["GET"])
+def libro_autor():
+    res = bbdd("SELECT author, count(*) FROM books GROUP BY author ORDER BY 2 DESC")
+    return jsonify(res)
 
 # 2.Ruta para obtener los libros de un autor
-
+@app.route("/buscar_libro", methods = ["GET"])
+def buscar_libro():
+    autor = request.args["autor"]
+    query = f"SELECT * FROM books WHERE author = '{autor}'"
+    res = bbdd(query)
+    return jsonify(res)
 # 3.Ruta para añadir un libro
+@app.route("/insertar_libro", methods = ["POST"])
+def insertar_libro():
+    """
+    {
+    [,]
+    }
+    """
+
+app.run()
---
+++ 4_Data_Engineering/1-APIs/Model/ejercicio/app.py
@@ -0,0 +1,5 @@
+# 1. Ofrezca la predicción de ventas a partir de todos los valores de gastos en publicidad. (/predict)
+
+# 2. Un endpoint para almacenar nuevos registros en la base de datos que deberás crear previamente.(/ingest)
+
+# 3. Posibilidad de reentrenar de nuevo el modelo con los posibles nuevos registros que se recojan. (/retrain)
